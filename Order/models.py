6320696c5a772497fefbb5cc9e76aa9ce295b117
unknown <matheus.fcarvalho2001@gmail.com>
2023-06-23 15:05:32 -0300

    remove order

--- Order/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Order(models.Model):
-    id = models.IntegerField(primary_key=True)
-    client = models.CharField(max_length=100, default='não especificado')
-    is_ready = models.BooleanField(default=False)
-
-    def __str__(self):
-        return str(self.id)
--- Order/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import SellersPage, ConfirmOrder
-
-app_name = 'order'
-
-urlpatterns = [
-    path('sellers/', SellersPage, name='create'),
-    path('confirm/', ConfirmOrder, name='confirm'),
-]
